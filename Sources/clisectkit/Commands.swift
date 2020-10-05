027d1cfd4c3be07a4326ff1fbc576de35114c129
Drew Crawford <drew@sealedabstract.com>
2020-10-05 16:39:47 -0500

    Adjust argument parsing

--- Sources/clisectkit/Commands.swift
+++ Sources/clisectkit/Commands.swift
@@ -3,14 +3,15 @@ import ArgumentParser
 enum Errors: Error {
     case invalidType(String)
 }
-enum FormatType {
-    init(argument: String) throws {
+enum FormatType: ExpressibleByArgument,CaseIterable {
+    init?(argument: String) {
         switch argument {
         case "double", "d": self = .double
-        case "int","i": self = .int
-        default: throw Errors.invalidType(argument)
+        case "int": self = .int
+        default: return nil
         }
     }
+    
     
     case double
     case int
@@ -21,7 +22,11 @@ struct Clisect: ParsableCommand {
     @Argument(help: "Range to bisect")
     var range: String
     
-    @Option(transform: FormatType.init)
+    @Option(help: ArgumentHelp("The type to bisect", discussion: """
+    d,double: range is in doubles
+    i,int: range is an int
+    """
+    , valueName: "format"))
     var formatType: FormatType = .double
     
     func run() throws {
